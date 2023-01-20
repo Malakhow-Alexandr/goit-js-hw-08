8b2eba9e62c202279d4be64677c6b3f43039e771
Malakhow-Alexandr <109951660+Malakhow-Alexandr@users.noreply.github.com>
2023-01-21 00:13:35 +0200

    remove read me md

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,7 +5,6 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
 
